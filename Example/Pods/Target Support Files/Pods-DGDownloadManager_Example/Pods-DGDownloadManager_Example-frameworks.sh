8a079fbdf914ad04132a3c75a2a90a2eaec5ef0d
hzy <hzycode@163.com>
2021-03-06 22:57:31 +0400

    提交

--- Example/Pods/Target Support Files/Pods-DGDownloadManager_Example/Pods-DGDownloadManager_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DGDownloadManager_Example/Pods-DGDownloadManager_Example-frameworks.sh
@@ -175,9 +175,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DGDownloadManager/DGDownloadManager.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DGDownloadManager/DGDownloadManager.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
